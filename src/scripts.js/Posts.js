5baf0b1412b5ef944d80ff0336a0abf7a2c575d9
Natan__Brito <natanismaelbrito@gmail.com>
2022-03-13 21:48:28 -0300

    array de objetos mais organizada

--- src/scripts.js/Posts.js
+++ src/scripts.js/Posts.js
@@ -1,14 +1,14 @@
 import Post from "./Post"
 export default function Posts(){
-    const infos=[{
+    const infos=[{user:{
         smallImg:"./img/meowed.svg",
         nameSmallImg:"meowed",
         bigImg:"./img/gato-telefone.svg",
         likeImg:"./img/respondeai.svg",
         likeName:"respondeai",
         likeNumber:101.523
-    },
-    {
+    }},
+     {user:{
         smallImg:"./img/barked.svg",
         nameSmallImg:"barked",
         bigImg:"./img/dog.svg",
@@ -16,12 +16,12 @@ export default function Posts(){
         likeName:"adorable_animals",
         likeNumber:99.159
     }
-   ]
+    }]
     return(
     infos.map((info) =>{
         return(
-        <Post smallImg={info.smallImg} nameSmallImg={info.nameSmallImg} bigImg={info.bigImg}
-     likeImg={info.likeImg} likeName={info.likeName} likeNumber={info.likeNumber} />
+        <Post smallImg={info.user.smallImg} nameSmallImg={info.user.nameSmallImg} bigImg={info.user.bigImg}
+     likeImg={info.user.likeImg} likeName={info.user.likeName} likeNumber={info.user.likeNumber} />
         )
     })
     )
