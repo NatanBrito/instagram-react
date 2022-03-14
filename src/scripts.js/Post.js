2f92720cbe02c2cfd4c603c5426a9f2256a612ef
Natan__Brito <natanismaelbrito@gmail.com>
2022-03-13 21:42:56 -0300

    primeira versÃo de entrega

---
+++ src/scripts.js/Post.js
@@ -0,0 +1,39 @@
+export default function Post({smallImg,nameSmallImg,bigImg,likeImg,likeName,likeNumber}){
+
+    return(
+        <div className="post">
+              <div className="topo">
+                <div className="usuario">
+                  <img src={smallImg} />
+                  {nameSmallImg}
+                </div>
+                <div className="acoes">
+                  <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+              </div>
+
+              <div className="conteudo">
+                <img src={bigImg} />
+              </div>
+
+              <div className="fundo">
+                <div className="acoes">
+                  <div>
+                    <ion-icon name="heart-outline"></ion-icon>
+                    <ion-icon name="chatbubble-outline"></ion-icon>
+                    <ion-icon name="paper-plane-outline"></ion-icon>
+                  </div>
+                  <div>
+                    <ion-icon name="bookmark-outline"></ion-icon>
+                  </div>
+                </div>
+                <div className="curtidas">
+                  <img src={likeImg} />
+                  <div className="texto">
+                    Curtido por <strong>{likeName}</strong> e <strong>outras {likeNumber} pessoas</strong>
+                  </div>
+                </div>
+              </div>
+            </div>
+    )
+}
--- src/scripts.js/Posts.js
+++ src/scripts.js/Posts.js
@@ -1,71 +1,28 @@
+import Post from "./Post"
 export default function Posts(){
-  const infos=[{
-    topoImg:"./img/meowed.svg",iconName:"ellipsis-horizontal",conteudoImg:"./img/gato-telefone.svg",
-    curtidaImg:"./img/respondeai.svg",curtidaName:"respondeai",like:101.523}]
+    const infos=[{
+        smallImg:"./img/meowed.svg",
+        nameSmallImg:"meowed",
+        bigImg:"./img/gato-telefone.svg",
+        likeImg:"./img/respondeai.svg",
+        likeName:"respondeai",
+        likeNumber:101.523
+    },
+    {
+        smallImg:"./img/barked.svg",
+        nameSmallImg:"barked",
+        bigImg:"./img/dog.svg",
+        likeImg:"./img/adorable_animals.svg",
+        likeName:"adorable_animals",
+        likeNumber:99.159
+    }
+   ]
     return(
-        <div className="posts">
-        <div className="post">
-        <Topo imgs="./img/meowed.svg" name="ellipsis-horizontal"/>
-        <Conteudo imgs="./img/gato-telefone.svg" />
-        <div className="fundo">
-        <Acoes />
-        <Curtidas imgs="./img/respondeai.svg" name="respondeai" like="101.523" />
-        </div>
-        </div>
-         <div className="post">
-        <Topo imgs="./img/barked.svg" name="ellipsis-horizontal"/>
-        <Conteudo imgs="./img/dog.svg" />
-        <div className="fundo">
-        <Acoes />
-        <Curtidas imgs="./img/adorable_animals.svg" name="adorable_animals" like="99.159" />
-        </div>
-        </div>
-
-        </div>
-    )
-    
-}
-function Topo(props){
-    return(
-    <div className="topo">
-                <div className="usuario">
-                  <img src={props.imgs} />
-                  meowed
-                </div>
-                <div className="acoes">
-                  <ion-icon name={props.name}></ion-icon>
-                </div>
-    </div>
-    )
-}
-function Conteudo(props){
-    return(
-        <div className="conteudo">
-                <img src={props.imgs} />
-              </div>
-    )
-}
-function Acoes(){
-    return(
-        <div className="acoes">
-        <div>
-          <ion-icon name="heart-outline"></ion-icon>
-          <ion-icon name="chatbubble-outline"></ion-icon>
-          <ion-icon name="paper-plane-outline"></ion-icon>
-        </div>
-        <div>
-          <ion-icon name="bookmark-outline"></ion-icon>
-        </div>
-      </div>
-    )
-}
-function Curtidas(props){
-    return (
-        <div className="curtidas">
-        <img src={props.imgs} />
-        <div className="texto">
-          Curtido por <strong>{props.name}</strong> e <strong>outras {props.like} pessoas</strong>
-        </div>
-      </div>    
+    infos.map((info) =>{
+        return(
+        <Post smallImg={info.smallImg} nameSmallImg={info.nameSmallImg} bigImg={info.bigImg}
+     likeImg={info.likeImg} likeName={info.likeName} likeNumber={info.likeNumber} />
+        )
+    })
     )
 }
